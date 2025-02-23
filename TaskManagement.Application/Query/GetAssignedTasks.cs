edf27418cd49fb302fb0bee958b8567d9f5971a9
atesh bhalerao <bhalerao1214@gmail.com>
2025-02-23 18:52:45 +0530

    Getassigned task by id imp

---
+++ TaskManagement.Application/Query/GetAssignedTasks.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using TaskManagement.Domain.Entity;
+using TaskManagement.Domain.Repository;
+namespace TaskManagement.Application.Query
+{
+    public sealed class GetAssignedTasks
+    {
+        // Query returning a single string
+        public sealed record Query(int UserId) : IRequest<List<GetAssignedTaskByUserId>>;
+
+        public class Handler : IRequestHandler<Query, List<GetAssignedTaskByUserId>>
+        {
+            private readonly IRepository _repository;   
+            public Handler(IRepository repository)
+            {
+                _repository = repository;
+            }   
+            public async Task<List<GetAssignedTaskByUserId>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var result = await _repository.GetAssignedTasks(request.UserId);   
+                return result;
+            }
+        }
+    }
+}
